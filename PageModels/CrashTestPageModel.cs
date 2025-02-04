eef5b1d1da22550725a8d7f9323c7136df7f06c6
Kevin Zhao <59250@fenetwork.com>
2025-02-04 08:11:50 -0600

    test #2

--- PageModels/CrashTestPageModel.cs
+++ PageModels/CrashTestPageModel.cs
@@ -5,14 +5,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiAppCrashTest.PageModels;
 
 public partial class CrashTestPageModel : ObservableObject
 {
+  
     [RelayCommand]
     async Task Back()
     {
         await Shell.Current.GoToAsync($"..");
     }
+    private static async Task NavigatingBack()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+
+    public ICommand NavigateBack  => new Command(async () => await NavigatingBack());
 }
